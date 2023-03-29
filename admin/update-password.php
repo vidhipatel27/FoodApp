49dabad9c05ee8965fdb0ac626d91280e43f1f79
vidhipatel27 <vidhipatel1027@gmail.com>
2023-03-29 21:43:49 +0530

    by vidhi

---
+++ admin/update-password.php
@@ -0,0 +1,11 @@
+<?php include('partials/menu.php');?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Change Password</h1>
+        <br><br>
+    </div>
+
+</div>
+
+<?php include('partials/footer.php');?>
